4f9144a1c2fe0d30c4f515783045c6e2b3cecb8d
EkaterinaEl <hackerrussia@yandex.ru>
2023-06-06 14:20:59 +0500

    Added viewModel for list

--- app/src/main/java/com/elka/shopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/elka/shopinglist/data/ShopListRepositoryImpl.kt
@@ -7,6 +7,13 @@ object ShopListRepositoryImpl : ShopListRepository {
   private val items = mutableListOf<ShopItem>()
   private var autoIncrementId = 0
 
+  init {
+    for(i in 0 until 10) {
+      val item = ShopItem("Name #$i", i, true)
+      addShopItem(item)
+    }
+  }
+
   override fun getShopList(): List<ShopItem> {
     return items.toList()
   }
--- app/src/main/java/com/elka/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/elka/shopinglist/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.elka.shopinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.elka.shopinglist.R
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var viewModel: MainViewModel
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+    viewModel.shopList.observe(this) {
+      Log.d("SHOP_LIST", it.toString())
+    }
+    viewModel.getShopList()
+
   }
 }
